1f0e4cc5c2f20429f366f9195bb311404962e9e6
emrea <emreakburak94@gmail.com>
2024-02-23 15:08:06 +0300

    Github actions test

--- DoneWithIt/test/LoginScreen.test.js
+++ DoneWithIt/test/LoginScreen.test.js
@@ -14,17 +14,5 @@ describe("LoginScreen", () => {
     expect(getByText("Login")).toBeTruthy();
   });
 
-  it("handles input change correctly", () => {
-    const { getByPlaceholderText } = render(<LoginScreen />);
-    const identityInput = getByPlaceholderText("Identity Number");
-    const passwordInput = getByPlaceholderText("Password");
-
-    fireEvent.changeText(identityInput, "1234567890");
-    fireEvent.changeText(passwordInput, "password123");
-
-    expect(identityInput.props.value).toBe("1234567890");
-    expect(passwordInput.props.value).toBe("password123");
-  });
-
   // You can write more test cases to cover different scenarios
 });
