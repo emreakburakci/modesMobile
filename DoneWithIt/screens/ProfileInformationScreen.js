308a8d1809837a349ceded783cb3b04bda2de38f
emrea <emreakburak94@gmail.com>
2024-02-29 14:00:24 +0300

    Profile Information Screen Notification Badge

--- DoneWithIt/screens/ProfileInformationScreen.js
+++ DoneWithIt/screens/ProfileInformationScreen.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { getUnreadNotificationsCount } from "../utils/NotificationUtils";
+import { useIsFocused } from "@react-navigation/native"; // Import useIsFocused hook
 
 import { useGlobalState } from "../GlobalStateContext";
 import { getTranslationResource } from "../utils/LanguageUtils";
@@ -12,7 +14,21 @@ const ProfileInformationScreen = ({ navigation, route }) => {
   console.log("USER INFO:", userInfo);
   console.log("COUNTRY:", userInfo.country);
   let translations = getTranslationResource(globalState.language);
+  const [unreadNotificationsCount, setUnreadNotificationsCount] = useState(0);
+  const isFocused = useIsFocused(); // Check if the screen is focused
 
+  useEffect(() => {
+    // Fetch unread notification count when the screen is focused
+    const fetchUnreadNotifications = async () => {
+      const data = await getUnreadNotificationsCount(
+        globalState.identityNumberGlobal
+      );
+      setUnreadNotificationsCount(data.count);
+    };
+    if (isFocused) {
+      fetchUnreadNotifications();
+    }
+  }, [isFocused]);
   const fetchData = () => {
     const user = getUserInformation(globalState.identityNumberGlobal);
     console.log(user);
@@ -93,7 +109,14 @@ const ProfileInformationScreen = ({ navigation, route }) => {
           style={styles.toolbarButton}
           onPress={handleNotificationButton}
         >
-          <Feather name="bell" size={24} style={styles.icon} />
+          <View style={styles.iconWithBadge}>
+            <Feather name="bell" size={24} style={styles.icon} />
+          </View>
+          {unreadNotificationsCount > 0 && (
+            <View style={styles.badge}>
+              <Text style={styles.badgeText}>{unreadNotificationsCount}</Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -184,6 +207,28 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     resizeMode: "contain",
     marginTop: 20,
+  },
+  iconWithBadge: {
+    position: "relative",
+    width: 24,
+    height: 24,
+  },
+  badge: {
+    position: "absolute",
+    top: -8,
+    right: 4,
+    backgroundColor: "red",
+    borderRadius: 10,
+    minWidth: 20,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  badgeText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 12,
   },
 });
 export default ProfileInformationScreen;
